d8f3469f8a4a85fd331eec253bd1f12d20b9ec7a
Salim Oussayfi <box@slimou.net>
2022-03-20 11:27:38 +0100

    validation via class

---
+++ VALIDATOR_CONVERTER/src/main/java/resources/EmailValidator.java
@@ -0,0 +1,27 @@
+package resources;
+
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value = "emailValidator")
+public class EmailValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext context, UIComponent uic, Object object) throws ValidatorException {
+
+		String value = (String) object;
+		final String EMAIL_PATTERN = "^[a-zA-Z0-9_.+-]+@[a-zA-z0-9- ]+\\.[a-zA-z0-9-.]+$";
+		
+		if (!Pattern.matches(EMAIL_PATTERN, value)) {
+			FacesMessage fm = new FacesMessage("Wrong email input");
+			throw new ValidatorException(fm);
+		}  
+	}
+
+}
